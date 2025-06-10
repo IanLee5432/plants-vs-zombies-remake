1d4c0528f7b8397278c24fb653143fa44c709c8c
IanLee5432 <ian.lee@student.csd509j.net>
2025-06-10 16:48:51 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,9 @@
 namespace SpriteKind {
-    export const plant = SpriteKind.create()
+    export const sunflower = SpriteKind.create()
     export const pea_shooter_plant = SpriteKind.create()
     export const moneycounterclass = SpriteKind.create()
     export const Snow_golem_class = SpriteKind.create()
+    export const Emerald_Class = SpriteKind.create()
 }
 function spawn_zombie () {
     Normal_Zombie = sprites.create(img`
@@ -23,9 +24,9 @@ function spawn_zombie () {
         . . . . e . . . . e e . . . . . 
         . . . . e . . . . . e e . . . . 
         `, SpriteKind.Enemy)
-    sprites.setDataNumber(Normal_Zombie, "Health", 16)
+    sprites.setDataNumber(Normal_Zombie, "Health", 11)
     tiles.placeOnTile(Normal_Zombie, tiles.getTileLocation(10, randint(1, 5)))
-    Normal_Zombie.setVelocity(-3, 0)
+    sprites.setDataNumber(Normal_Zombie, "Speed", -2.5)
 }
 // Fix delay logic
 controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
@@ -33,23 +34,25 @@ controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
 })
 // Placing the plants
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-    if (cursor.overlapsWith(emerald)) {
-        sprites.destroy(emerald)
-        money += 25
+    for (let value of sprites.allOfKind(SpriteKind.Emerald_Class)) {
+        if (cursor.overlapsWith(value)) {
+            sprites.destroy(value)
+            money += 25
+        }
     }
-    if (placingPlant && (cursor.tileKindAt(TileDirection.Center, assets.tile`myTile1`) || cursor.tileKindAt(TileDirection.Center, assets.tile`myTile0`))) {
+    if (placingPlant && (current_plant == "pea shooter" && (cursor.tileKindAt(TileDirection.Center, assets.tile`myTile1`) || cursor.tileKindAt(TileDirection.Center, assets.tile`myTile0`)))) {
         placingPlant = false
         sprites.setDataBoolean(pea_shooter, "is_placed", true)
         current_plant = ""
     }
-    if (placingPlant && (cursor.tileKindAt(TileDirection.Center, assets.tile`myTile1`) || cursor.tileKindAt(TileDirection.Center, assets.tile`myTile0`))) {
+    if (placingPlant && (current_plant == "sunflower" && (cursor.tileKindAt(TileDirection.Center, assets.tile`myTile1`) || cursor.tileKindAt(TileDirection.Center, assets.tile`myTile0`)))) {
         placingPlant = false
         current_plant = ""
     }
-    if (placingPlant && (cursor.tileKindAt(TileDirection.Center, assets.tile`myTile1`) || cursor.tileKindAt(TileDirection.Center, assets.tile`myTile0`))) {
+    if (placingPlant && (current_plant == "snow golem" && (cursor.tileKindAt(TileDirection.Center, assets.tile`myTile1`) || cursor.tileKindAt(TileDirection.Center, assets.tile`myTile0`)))) {
         placingPlant = false
-        sprites.setDataBoolean(Snow_Golem, "is_placed", true)
         current_plant = ""
+        sprites.setDataBoolean(Snow_Golem, "is_placed", true)
     }
 })
 controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
@@ -65,11 +68,7 @@ function Level1 () {
     // middle waves
     for (let index2 = 0; index2 <= 2; index2++) {
         timer.after(90000 + index2 * 1500, function () {
-            for (let index3 = 0; index3 <= 1; index3++) {
-                timer.after(index3 * 1000, function () {
-                    spawn_zombie()
-                })
-            }
+        	
         })
     }
     timer.after(180000, function () {
@@ -100,17 +99,17 @@ sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, oth
         sprites.destroy(otherSprite)
     }
 })
-let Sunflower: Sprite = null
-let projectile: Sprite = null
-let Snowball: Sprite = null
-let Snow_Golem: Sprite = null
-let current_plant = ""
-let pea_shooter: Sprite = null
-let placingPlant = false
 let emerald: Sprite = null
+let snowball: Sprite = null
+let projectile: Sprite = null
+let Sunflower: Sprite = null
+let Snow_Golem: Sprite = null
+let pea_shooter: Sprite = null
+let current_plant = ""
+let placingPlant = false
 let Normal_Zombie: Sprite = null
 let cursor: Sprite = null
-let money = 1000
+let money = 25
 tiles.setCurrentTilemap(tilemap`level1`)
 cursor = sprites.create(img`
     . . . . . . . . . . . . . . . . 
@@ -156,55 +155,8 @@ game.onUpdate(function () {
     money_counter.sayText(money, 500, false)
 })
 game.onUpdate(function () {
-    for (let value of sprites.allOfKind(SpriteKind.Snow_golem_class)) {
-        if (sprites.readDataBoolean(value, "is_placed")) {
-            if (sprites.readDataNumber(value, "next_shoot_time") <= game.runtime()) {
-                Snowball = sprites.createProjectileFromSprite(img`
-                    . . . . . . . . . . . . . . . . 
-                    . . . . . . . . . . . . . . . . 
-                    . . . . . . . . . . . . . . . . 
-                    . . . . . . 1 1 1 1 1 . 1 . . . 
-                    . . . . . . 1 1 . 1 1 1 1 . . . 
-                    . . . . . 1 1 1 1 1 1 1 1 . . . 
-                    . . . . . 1 1 1 1 1 1 1 1 . . . 
-                    . . . . 1 1 1 1 1 1 1 . 1 . . . 
-                    . . . . 1 1 1 1 1 1 1 . 1 . . . 
-                    . . . . 1 1 1 1 1 1 1 1 . . . . 
-                    . . . . . 1 1 1 1 1 1 . . . . . 
-                    . . . . . . . . . . . . . . . . 
-                    . . . . . . . . . . . . . . . . 
-                    . . . . . . . . . . . . . . . . 
-                    . . . . . . . . . . . . . . . . 
-                    . . . . . . . . . . . . . . . . 
-                    `, value, 75, 0)
-                sprites.setDataNumber(value, "next_shoot_time", game.runtime() + randint(1700, 2000))
-            }
-        }
-    }
-    for (let value of sprites.allOfKind(SpriteKind.pea_shooter_plant)) {
-        if (sprites.readDataBoolean(value, "is_placed")) {
-            if (sprites.readDataNumber(value, "next_shoot_time") <= game.runtime()) {
-                projectile = sprites.createProjectileFromSprite(img`
-                    . . . . . . . . . . . . . . . . 
-                    . . . . . . . . . . . . . . . . 
-                    . . . . . . . . . . . . . . . . 
-                    . . . . . . a a a a a . a . . . 
-                    . . . . . . a a . a a a a . . . 
-                    . . . . . a a a a a a a a . . . 
-                    . . . . . a a a a a a a a . . . 
-                    . . . . a a a a a a a . a . . . 
-                    . . . . a a a a a a a . a . . . 
-                    . . . . a a a a a a a a . . . . 
-                    . . . . . a a a a a a . . . . . 
-                    . . . . . . . . . . . . . . . . 
-                    . . . . . . . . . . . . . . . . 
-                    . . . . . . . . . . . . . . . . 
-                    . . . . . . . . . . . . . . . . 
-                    . . . . . . . . . . . . . . . . 
-                    `, value, 75, 0)
-                sprites.setDataNumber(value, "next_shoot_time", game.runtime() + randint(1700, 2000))
-            }
-        }
+    for (let value of sprites.allOfKind(SpriteKind.Enemy)) {
+        value.setVelocity(sprites.readDataNumber(value, "Speed"), 0)
     }
 })
 // Makes the plant follow the cursor when the plant is picked up
@@ -247,10 +199,11 @@ game.onUpdate(function () {
                     . . . . . . . . 3 . . . . . . . 
                     . . . . . . . . 3 . . . . . . . 
                     . . . . . . . . 3 . . . . . . . 
-                    `, SpriteKind.plant)
+                    `, SpriteKind.sunflower)
                 placingPlant = true
                 money += -50
                 current_plant = "sunflower"
+                sprites.setDataNumber(Sunflower, "next_generation_time", game.runtime() + randint(5000, 8000))
             }
             if (cursor.tileKindAt(TileDirection.Center, assets.tile`myTile7`) && money >= 100) {
                 pea_shooter = sprites.create(img`
@@ -305,8 +258,85 @@ game.onUpdate(function () {
         }
     }
 })
+game.onUpdate(function () {
+    for (let value of sprites.allOfKind(SpriteKind.pea_shooter_plant)) {
+        if (sprites.readDataBoolean(value, "is_placed")) {
+            if (sprites.readDataNumber(value, "next_shoot_time") <= game.runtime()) {
+                projectile = sprites.createProjectileFromSprite(img`
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . a a a a a . a . . . 
+                    . . . . . . a a . a a a a . . . 
+                    . . . . . a a a a a a a a . . . 
+                    . . . . . a a a a a a a a . . . 
+                    . . . . a a a a a a a . a . . . 
+                    . . . . a a a a a a a . a . . . 
+                    . . . . a a a a a a a a . . . . 
+                    . . . . . a a a a a a . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    `, value, 75, 0)
+                sprites.setDataNumber(value, "next_shoot_time", game.runtime() + randint(1700, 2000))
+            }
+        }
+    }
+    for (let value of sprites.allOfKind(SpriteKind.Snow_golem_class)) {
+        if (sprites.readDataBoolean(value, "is_placed")) {
+            if (sprites.readDataNumber(value, "next_shoot_time") <= game.runtime()) {
+                snowball = sprites.createProjectileFromSprite(img`
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . 1 1 1 1 1 . 1 . . . 
+                    . . . . . . 1 1 . 1 1 1 1 . . . 
+                    . . . . . 1 1 1 1 1 1 1 1 . . . 
+                    . . . . . 1 1 1 1 1 1 1 1 . . . 
+                    . . . . 1 1 1 1 1 1 1 . 1 . . . 
+                    . . . . 1 1 1 1 1 1 1 . 1 . . . 
+                    . . . . 1 1 1 1 1 1 1 1 . . . . 
+                    . . . . . 1 1 1 1 1 1 . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    `, value, 75, 0)
+                sprites.setDataNumber(value, "next_shoot_time", game.runtime() + randint(1700, 2000))
+            }
+        }
+    }
+    for (let value of sprites.allOfKind(SpriteKind.sunflower)) {
+        if (sprites.readDataNumber(value, "next_generation_time") <= game.runtime()) {
+            emerald = sprites.create(img`
+                . . . . . . . . . . . . . . . . 
+                . . . . . . 7 7 7 7 . . . . . . 
+                . . . . . 7 1 e 7 7 3 . . . . . 
+                . . . . 7 1 e e e 7 7 3 . . . . 
+                . . . 7 1 e e e e e e 7 3 . . . 
+                . . . 7 e e e 1 e e e 7 3 . . . 
+                . . . 7 e e e 1 e e e 7 3 . . . 
+                . . . 7 e e e 1 e e e 7 3 . . . 
+                . . . 7 e e e 1 e e 7 7 3 . . . 
+                . . . 7 e e 1 e e 7 7 7 3 . . . 
+                . . . . 7 e e e 7 7 7 3 . . . . 
+                . . . . . 7 e e 3 3 3 . . . . . 
+                . . . . . . 7 3 3 3 . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                `, SpriteKind.Emerald_Class)
+            emerald.setPosition(value.x, value.y)
+            emerald.setVelocity(0, 0)
+            sprites.setDataNumber(value, "next_generation_time", game.runtime() + randint(20000, 25000))
+        }
+    }
+})
 game.onUpdateInterval(randint(8000, 12000), function () {
-    emerald = sprites.createProjectileFromSide(img`
+    emerald = sprites.create(img`
         . . . . . . . . . . . . . . . . 
         . . . . . . 7 7 7 7 . . . . . . 
         . . . . . 7 1 e 7 7 3 . . . . . 
@@ -323,6 +353,7 @@ game.onUpdateInterval(randint(8000, 12000), function () {
         . . . . . . . . . . . . . . . . 
         . . . . . . . . . . . . . . . . 
         . . . . . . . . . . . . . . . . 
-        `, 0, 10)
+        `, SpriteKind.Emerald_Class)
     tiles.placeOnTile(emerald, tiles.getTileLocation(randint(1, 9), 0))
+    emerald.setVelocity(0, 10)
 })
